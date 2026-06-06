815cf36d87983efbb19fb54b7a5c0347b840624e
Venkxt-0 <25cs239@rmkec.ac.in>
2026-06-06 10:08:59 +0530

    Create Combination_Generator.cpp

---
+++ Combination_Generator.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void solve() {
+    string s;
+    cout << "Enter a string: ";
+    cin >> s; 
+    
+    int L = s.length();
+  
+    for (int ctr = 1; ctr < (1 << L); ctr++) {
+        string combi = "";
+        
+     
+        for (int sh = 0; sh < L; sh++) {
+         
+            if ((ctr & (1 << sh)) > 0) {
+                combi += s[sh]; 
+            }
+        }
+        
+        cout << combi << endl;
+    }
+}
+
+int main() {
+    solve();
+    return 0;
+}
